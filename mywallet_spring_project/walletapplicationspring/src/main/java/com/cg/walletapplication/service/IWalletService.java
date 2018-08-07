8d0d71f50d52e71d5d45eb058b9b80724f92c735
saisumanth93 <41038294+saisumanth93@users.noreply.github.com>
2018-08-07 10:51:15 +0530

    Add files via upload

---
+++ mywallet_spring_project/walletapplicationspring/src/main/java/com/cg/walletapplication/service/IWalletService.java
@@ -0,0 +1,26 @@
+package com.cg.walletapplication.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
+
+import com.cg.walletapplication.beans.Customer;
+import com.cg.walletapplication.beans.Transaction;
+
+public interface IWalletService {
+
+	void addCustomer(Customer customer);
+
+	BigDecimal showBalance(String mobileno);
+
+	String deposit(String mobileno, String password, BigDecimal amount);
+
+	Customer showCustomer(String mobileno);
+
+	String withdraw(String mobileno, String password, BigDecimal amount);
+
+	String fundtransfer(String targetmobileno, String sourcemobileno, String password, BigDecimal amount);
+
+	List<Transaction> printTransactions(String mobileno, String password);
+
+}
